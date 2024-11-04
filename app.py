175534c38c3f3d8d636cc486eaf7905cc7064acc
stanfrbd <stanfrbd@github.com>
2024-11-04 13:21:56 +0100

    Edited template, add filters and search options. Use multithread

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
 from engines import abuseipdb, virustotal, ipinfo, spur_us, reverse_dns, google_safe_browsing
 from utils import *
-from flask import Flask, request, render_template, send_file, redirect, url_for, jsonify
+from flask import Flask, request, render_template, send_file, jsonify
 import pandas as pd
 import threading
+import time
+import queue
 
 app = Flask(__name__)
 
-# Variable globale pour stocker les résultats d'analyse
+# Global variable to store analysis results
 results = []
-analysis_in_progress = False  # Variable pour suivre l'état de l'analyse
+analysis_in_progress = False  # Variable to track the state of the analysis
+analysis_metadata = {}
 
 def perform_analysis(observables, selected_engines):
-    global results, analysis_in_progress
-    results = []  # Réinitialiser les résultats à chaque analyse
+    start_time = time.time()
+    global results, analysis_in_progress, analysis_metadata
+    results = []  # Reset results for each analysis
     analysis_in_progress = True
 
-    for observable in observables:
-        observable_type = identify_observable_type(observable.strip())
-        result = {"observable": observable.strip(), "type": observable_type}
+    result_queue = queue.Queue()
 
-        # Assume that the reverse DNS will fail unless it succeeds
+    def analyze_observable(observable, observable_type, index):
+        result = {"observable": observable.strip(), "type": observable_type}
         result['reversed_success'] = False
-        
-        # Vérifier chaque engine sélectionné
+
         if "virustotal" in selected_engines and observable_type in ["MD5", "SHA1", "SHA256", "URL", "FQDN", "IPv4", "IPv6"]:
             result['virustotal'] = virustotal.query_virustotal(observable.strip())
 
@@ -33,12 +35,10 @@ def perform_analysis(observables, selected_engines):
             result['reverse_dns'] = reverse_dns.reverse_dns(observable.strip())
             if observable_type == "FQDN" and result['reverse_dns'] is not None:
                 result['reversed_success'] = True
-                observable_type = "IPv4"  # Change the observable type to IP for further analysis
-                observable = result["reverse_dns"]["reverse_dns"][-1] # Change the observable to the resolved IP
-            
+                observable_type = "IPv4"
+                observable = result["reverse_dns"]["reverse_dns"][-1]
 
         if "ipinfo" in selected_engines and observable_type in ["IPv4", "IPv6"]:
-            # if observable is FQDN, we need to resolve it to an IP first
             result['ipinfo'] = ipinfo.query_ipinfo(observable.strip())
 
         if "abuseipdb" in selected_engines and observable_type in ["IPv4", "IPv6"]:
@@ -47,41 +47,70 @@ def perform_analysis(observables, selected_engines):
         if "spur" in selected_engines and observable_type in ["IPv4", "IPv6"]:
             result['spur'] = spur_us.process_ip_with_spur(observable.strip())
 
-        results.append(result)
+        result_queue.put((index, result))
 
-    print("Résultats d'analyse:", results)
-    analysis_in_progress = False  # Fin de l'analyse
+    threads = []
+    for index, observable in enumerate(observables):
+        observable_type = identify_observable_type(observable.strip())
+        thread = threading.Thread(target=analyze_observable, args=(observable, observable_type, index))
+        threads.append(thread)
+        thread.start()
 
-# Route principale pour l'analyse
+    for thread in threads:
+        thread.join()
+
+    # Collect results from the queue and sort by index
+    results = [None] * len(observables)
+    while not result_queue.empty():
+        index, result = result_queue.get()
+        results[index] = result
+
+    end_time = time.time()
+    
+    start_time_string = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))
+    end_time_string = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(end_time))
+
+    analysis_duration = end_time - start_time
+    # convert analysis duration to minutes and seconds
+    analysis_duration_string = f"{int(analysis_duration // 60)} minutes, {analysis_duration % 60:.2f} seconds"
+    
+    analysis_metadata = {"start_time": start_time, "end_time": end_time, "start_time_string": start_time_string, "end_time_string": end_time_string ,"analysis_duration_string": analysis_duration_string, "analysis_duration": analysis_duration}
+    analysis_in_progress = False  # End of analysis
+    # Existing code to perform analysis
+    print("Analysis results:", results)
+    print("Analysis metadata:", analysis_metadata)
+    print(f"Analysis took {analysis_metadata['analysis_duration_string']}")
+
+# Main route for analysis
 @app.route('/')
 def index():
     return render_template('index.html', results=[])
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
-    observables = request.form.get("observables", "").splitlines()
+    observables = [obs for obs in request.form.get("observables", "").splitlines() if obs.strip()]
     selected_engines = request.form.getlist("engines")
 
-    # Lancer l'analyse dans un thread pour ne pas bloquer
+    # Start analysis in a thread to avoid blocking
     analysis_thread = threading.Thread(target=perform_analysis, args=(observables, selected_engines))
     analysis_thread.start()
 
-    # Afficher la page d'attente
+    # Display the waiting page
     return render_template('waiting.html'), 200  # Return 200 status for loading
 
 @app.route('/results', methods=['GET'])
 def show_results():
-    return render_template('index.html', results=results)
+    return render_template('index.html', results=results, analysis_metadata=analysis_metadata)
 
 @app.route('/is_analysis_complete', methods=['GET'])
 def is_analysis_complete():
     return jsonify({'complete': not analysis_in_progress})
 
-# Route pour exporter les résultats en CSV ou Excel
+# Route to export results in CSV or Excel
 @app.route('/export')
 def export():
     format = request.args.get('format')
-    df = pd.DataFrame(results)  # Convertir les résultats en DataFrame
+    df = pd.DataFrame(results)  # Convert results to DataFrame
 
     if format == 'csv':
         csv_path = 'results.csv'
