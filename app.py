76e1527a259e7f570883f47b8d3c72aa90829506
stanfrbd <stanfrbd@github.com>
2024-11-07 15:45:37 +0100

    true fix of favicon

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from engines import abuseipdb, virustotal, ipinfo, spur_us, reverse_dns, google_safe_browsing
 from utils import *
-from flask import Flask, request, render_template, send_file, jsonify
+from flask import Flask, request, render_template, send_file, jsonify, send_from_directory
 import pandas as pd
 import threading
 import time
@@ -176,6 +176,11 @@ def export():
         # Delete the file after 10 seconds
         threading.Thread(target=lambda path: (time.sleep(10), os.remove(path)), args=(excel_path,)).start()
         return response
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'images'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
