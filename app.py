ee608f0e52dd0e91885a4206b3fdfa3a84831d6c
stanfrbd <stanfrbd@github.com>
2024-11-29 09:57:49 +0100

    Add 404 and 500 handling

--- app.py
+++ app.py
@@ -199,5 +199,15 @@ def favicon():
     """Serve the favicon."""
     return send_from_directory(os.path.join(app.root_path, 'images'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    """Handle 404 errors."""
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    """Handle 500 errors."""
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
