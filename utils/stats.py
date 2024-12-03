d080599e4e9fbfb85780aa610a593f5957950584
stanfrbd <stanfrbd@github.com>
2024-12-03 15:47:11 +0100

    Refactoring for stats and database operations

---
+++ utils/stats.py
@@ -0,0 +1,47 @@
+from models.analysis_result import AnalysisResult, db
+
+def get_analysis_stats():
+    """Get analysis statistics from the database."""
+    analyses = AnalysisResult.query.all()
+    num_analyses = len(analyses)
+    
+    unique_observables = set()
+    unique_engines = set()
+    observable_type_count = {}
+    engine_count = {}
+    observable_count = {}
+
+    for analysis in analyses:
+        for result in analysis.results:
+            observable = result["observable"]
+            observable_type = result["type"]
+            unique_observables.add(observable)
+            if observable_type in observable_type_count:
+                observable_type_count[observable_type] += 1
+            else:
+                observable_type_count[observable_type] = 1
+
+            if observable in observable_count:
+                observable_count[observable] += 1
+            else:
+                observable_count[observable] = 1
+
+        for engine in analysis.selected_engines:
+            unique_engines.add(engine)
+            if engine in engine_count:
+                engine_count[engine] += 1
+            else:
+                engine_count[engine] = 1
+
+    stats = {
+        "num_analyses": num_analyses,
+        "num_unique_observables": len(unique_observables),
+        "unique_observables": list(unique_observables),
+        "num_unique_engines": len(unique_engines),
+        "unique_engines": list(unique_engines),
+        "observable_type_count": observable_type_count,
+        "engine_count": engine_count,
+        "observable_count": observable_count
+    }
+
+    return stats
