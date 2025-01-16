8a2d022c0cdd65b0754cde8bae868c2518aa72c6
stanfrbd <stanfrbd@github.com>
2025-01-16 11:15:11 +0100

    Add timeout for ThreatFox since the API has issues

--- engines/threatfox.py
+++ engines/threatfox.py
@@ -29,7 +29,7 @@ def query_threatfox(observable, observable_type, PROXIES):
     payload = {"query": "search_ioc", "search_term": observable}
 
     try:
-        response = requests.post(url, data=json.dumps(payload), proxies=PROXIES, verify=False, timeout=10)
+        response = requests.post(url, data=json.dumps(payload), proxies=PROXIES, verify=False, timeout=5)
         response.raise_for_status()
         result = response.json()
 
