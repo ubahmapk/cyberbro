f41df7b468be1b24ddf6f496346a80c9976ec086
stanfrbd <stanfrbd@github.com>
2025-03-09 22:46:05 +0100

    Upgrade to v0.6.1

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ app.config['SQLALCHEMY_POOL_SIZE'] = 10
 app.config['SQLALCHEMY_MAX_OVERFLOW'] = 20
 
 # Set version 
-app.config['VERSION'] = "v0.6.0"
+app.config['VERSION'] = "v0.6.1"
 
 # Initialize the database
 db.init_app(app)
