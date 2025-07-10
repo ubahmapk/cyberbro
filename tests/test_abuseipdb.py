44163106897454de8992ac9e1d0c740467547310
Jon Mark Allen <ubahmapk@gmail.com>
2025-07-10 12:08:03 -0500

    pytests for abuseipdb engine

--- tests/test_abuseipdb.py
+++ tests/test_abuseipdb.py
@@ -1,16 +1,17 @@
+import pytest
 import requests
-import sys
+from requests.exceptions import HTTPError
+import responses
+from utils.config import QueryError
 
 from pytest_mock import MockerFixture
 
-# sys.path.append("engines")
-
-from engines.abuseipdb import query_abuseipdb
+from engines.abuseipdb import parse_abuseipdb_response, query_abuseipdb
 
 
-def test_successful_api_query(mocker: MockerFixture):
-    mock_response = mocker.Mock()
-    mock_response.json.return_value = {
+@pytest.fixture()
+def valid_api_response():
+    return {
         "data": {
             "ipAddress": "1.1.1.1",
             "isPublic": True,
@@ -28,40 +29,50 @@ def test_successful_api_query(mocker: MockerFixture):
             "lastReportedAt": "2025-04-22T13:01:09+00:00",
         }
     }
-    mocker.patch("requests.get", return_value=mock_response)
 
-    result = query_abuseipdb("1.1.1.1", "test_key", None)
 
-    assert result == {
+@responses.activate
+def test_query_abuseipdb(valid_api_response, ip_observable_dict, api_key):
+    responses.add(responses.GET, "https://api.abuseipdb.com/api/v2/check", json=valid_api_response)
+    ip: str = ip_observable_dict["value"]
+
+    response = query_abuseipdb(ip, api_key, None, False)
+
+    assert response == valid_api_response
+
+
+@responses.activate
+def test_query_abuseipdb_http_error(api_key, ip_observable_dict):
+    ip: str = ip_observable_dict["value"]
+    responses.add(responses.GET, "https://api.abuseipdb.com/api/v2/check", body=HTTPError())
+
+    with pytest.raises(QueryError):
+        response = query_abuseipdb(ip, api_key, None, False)
+
+
+def test_request_timeout(ip_observable_dict, api_key, mocker: MockerFixture):
+    mocker.patch("requests.get", side_effect=requests.exceptions.Timeout)
+
+    with pytest.raises(QueryError):
+        result = query_abuseipdb(ip_observable_dict, api_key, None, False)
+
+
+def test_parse_abuseipdb_response(valid_api_response, ip_observable_dict):
+    ip: str = ip_observable_dict["value"]
+    expected: dict = {
         "reports": 24,
         "risk_score": 0,
         "link": "https://www.abuseipdb.com/check/1.1.1.1",
     }
 
+    report: dict | None = parse_abuseipdb_response(valid_api_response, ip)
 
-def test_request_timeout(mocker: MockerFixture):
-    mocker.patch("requests.get", side_effect=requests.exceptions.Timeout)
-
-    result = query_abuseipdb("1.1.1.1", "test_key", None)
-
-    assert result is None
+    assert report == expected
 
 
-def test_invalid_json_response(mocker: MockerFixture):
-    mock_response = mocker.Mock()
-    mock_response.json.side_effect = ValueError
-    mocker.patch("requests.get", return_value=mock_response)
+def test_missing_data_field_parse_abuseipdb(ip_observable_dict):
+    ip: str = ip_observable_dict["value"]
+    malformed_api_response: dict = {"not_a_data_field": "something else"}
 
-    result = query_abuseipdb("1.1.1.1", "test_key", None)
-
-    assert result is None
-
-
-def test_missing_data_field(mocker: MockerFixture):
-    mock_response = mocker.Mock()
-    mock_response.json.return_value = {"error": "No data"}
-    mocker.patch("requests.get", return_value=mock_response)
-
-    result = query_abuseipdb("1.1.1.1", "test_key", None)
-
-    assert result is None
+    with pytest.raises(QueryError):
+        report = parse_abuseipdb_response(malformed_api_response, ip)
