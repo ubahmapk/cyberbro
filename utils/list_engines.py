6937b84c8861423fd53e7b2db2addcd6e7d05c54
Jon Mark Allen <ubahmapk@gmail.com>
2025-07-10 12:03:31 -0500

    refactored into multiple functions for testing. Load engine modules dynamically

--- utils/list_engines.py
+++ utils/list_engines.py
@@ -1,85 +1,62 @@
-from engines import (
-    abuseipdb,
-    abusix,
-    alienvault,
-    chrome_extension,
-    criminalip,
-    crowdstrike,
-    github,
-    google,
-    google_dns,
-    google_safe_browsing,
-    hudsonrock,
-    ioc_one,
-    ipinfo,
-    ipquery,
-    microsoft_defender_for_endpoint,
-    misp,
-    opencti,
-    phishtank,
-    rdap,
-    reverse_dns,
-    shodan,
-    spur_us_free,
-    threatfox,
-    urlscan,
-    virustotal,
-    webscout,
-)
-from utils.config import Secrets, get_config
+import contextlib
+import importlib
+from types import SimpleNamespace
 
 
-def list_engines() -> dict[str, dict[str, str]]:
+class EngineModule(SimpleNamespace):
+    NAME: str
+    LABEL: str
+    DESCRIPTION: str
+    SUPPORTS: str
+    COST: str
+    API_KEY_REQUIRED: str
+    SUPPORTED_OBSERVABLE_TYPES: str
+
+
+def load_engines(selected_engines: list[str]) -> list[EngineModule]:
+    loaded_engines: list[EngineModule] = []
+
+    """
+    There's an inconsistency between the name listed in `gui_enabled_extensions`
+    and the filename for `chrome_extension.py`
+    """
+    if "extension" in selected_engines:
+        selected_engines = [engine for engine in selected_engines if engine != "extension"]
+        selected_engines.append("chrome_extension")
+
+    for engine in selected_engines:
+        with contextlib.suppress(ModuleNotFoundError):
+            loaded_engines.append(importlib.import_module(f"engines.{engine}"))  # type: ignore reportArgumentType
+
+    return loaded_engines
+
+
+def list_engines(selected_engines: list[str]) -> dict[str, dict[str, str]]:
+    loaded_engines: list[EngineModule] = load_engines(selected_engines)
+    return list_engine_metadata(loaded_engines)
+
+
+def list_engine_metadata(loaded_engines: list[EngineModule]) -> dict[str, dict[str, str]]:
     """
     Return a list of engines and their descriptions, based on each engine's metadata attributes.
     """
 
-    secrets: Secrets = get_config()
-
-    engines = [
-        abuseipdb,
-        abusix,
-        alienvault,
-        chrome_extension,
-        criminalip,
-        crowdstrike,
-        github,
-        google,
-        google_dns,
-        google_safe_browsing,
-        hudsonrock,
-        ioc_one,
-        ipinfo,
-        ipquery,
-        microsoft_defender_for_endpoint,
-        misp,
-        opencti,
-        phishtank,
-        rdap,
-        reverse_dns,
-        shodan,
-        spur_us_free,
-        threatfox,
-        urlscan,
-        virustotal,
-        webscout,
-    ]
-
     response: dict[str, dict[str, str]] = {}
 
     """Only return the engines that are enabled in the configuration."""
-    for engine in [engine for engine in engines if engine.NAME in secrets.gui_enabled_engines]:
-        response.update(
-            {
-                engine.NAME: {
-                    "label": engine.LABEL,
-                    "description": engine.DESCRIPTION,
-                    "supports": engine.SUPPORTS,
-                    "cost": engine.COST,
-                    "api_key_required": engine.API_KEY_REQUIRED,
-                    "supported_observable_types": engine.SUPPORTED_OBSERVABLE_TYPES,
+    for engine in loaded_engines:
+        with contextlib.suppress(AttributeError):
+            response.update(
+                {
+                    engine.NAME: {
+                        "label": engine.LABEL,
+                        "description": engine.DESCRIPTION,
+                        "supports": engine.SUPPORTS,
+                        "cost": engine.COST,
+                        "api_key_required": engine.API_KEY_REQUIRED,
+                        "supported_observable_types": engine.SUPPORTED_OBSERVABLE_TYPES,
+                    }
                 }
-            }
-        )
+            )
 
     return response
