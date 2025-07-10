15161d09f765d74bd739c49bbfdd30d886ec6719
Jon Mark Allen <ubahmapk@gmail.com>
2025-07-10 11:56:03 -0500

    updated observable parameter, refactored query and parsing response into separate functions for testing

--- engines/abuseipdb.py
+++ engines/abuseipdb.py
@@ -1,8 +1,9 @@
 import logging
 
 import requests
+from requests.exceptions import JSONDecodeError, RequestException
 
-from utils.config import Secrets, get_config
+from utils.config import APIKeyNotFoundError, QueryError, read_api_key
 
 logger = logging.getLogger(__name__)
 
@@ -16,7 +17,39 @@ COST: str = "Free"
 API_KEY_REQUIRED: bool = False
 
 
-def run_engine(ip: str, proxies: dict[str, str] | None, ssl_verify: bool = True) -> dict | None:
+def run_engine(observable: dict, proxies: dict[str, str] | None, ssl_verify: bool = True) -> dict | None:
+    """
+    Entrypoint for the AbuseIPDB engine.
+
+    Retrieves API key and queries the AbuseIPDB API for information about the given observable.
+
+    Return
+    """
+
+    try:
+        api_key: str = read_api_key(NAME)
+    except APIKeyNotFoundError as e:
+        logger.error(f"{LABEL} API key is not configured: %s", e)
+        return None
+
+    ip: str = observable["value"]
+
+    try:
+        response: dict = query_abuseipdb(
+            ip=ip,
+            api_key=api_key,
+            proxies=proxies,
+            ssl_verify=ssl_verify,
+        )
+        report: dict = parse_abuseipdb_response(response, ip)
+    except QueryError as e:
+        logger.error("Error querying AbuseIPDB: %s", e, exc_info=True)
+        return None
+
+    return report
+
+
+def query_abuseipdb(ip: str, api_key: str, proxies: dict[str, str] | None, ssl_verify: bool = True) -> dict:
     """
     Queries the AbuseIPDB API for information about a given IP address.
 
@@ -33,16 +66,13 @@ def run_engine(ip: str, proxies: dict[str, str] | None, ssl_verify: bool = True)
         None: If any error occurs or the response is missing necessary data.
 
     Raises:
-        requests.exceptions.RequestException: If there is an issue with the network request.
+        RequestException: If there is an issue with the network request.
         ValueError: If the response cannot be parsed as JSON.
     """
 
-    secrets: Secrets = get_config()
-    api_key: str = secrets.abuseipdb
-
-    url = "https://api.abuseipdb.com/api/v2/check"
-    headers = {"Key": api_key, "Accept": "application/json"}
-    params = {"ipAddress": ip}
+    url: str = "https://api.abuseipdb.com/api/v2/check"
+    headers: dict[str, str] = {"Key": api_key, "Accept": "application/json"}
+    params: dict[str, str] = {"ipAddress": ip}
 
     try:
         response: requests.Response = requests.get(
@@ -55,17 +85,21 @@ def run_engine(ip: str, proxies: dict[str, str] | None, ssl_verify: bool = True)
         )
         response.raise_for_status()  # Raises an HTTPError for 4xx/5xx statuses
         json_response = response.json()
-    except requests.exceptions.RequestException as req_err:
+    except (JSONDecodeError, RequestException) as req_err:
         logger.error("Network error while querying AbuseIPDB: %s", req_err, exc_info=True)
-        return None
+        raise QueryError from req_err
 
+    return json_response
+
+
+def parse_abuseipdb_response(json_response: dict, ip: str) -> dict:
     try:
         data = json_response["data"]
         reports = data.get("totalReports", 0)
         risk_score = data.get("abuseConfidenceScore", 0)
         link = f"https://www.abuseipdb.com/check/{ip}"
-
-        return {"reports": reports, "risk_score": risk_score, "link": link}
-    except (KeyError, TypeError):
+    except (KeyError, TypeError) as e:
         logger.warning("AbuseIPDB response has no 'data' key. Full response: %s", json_response)
-        return None
+        raise QueryError("Invalid response format from AbuseIPDB") from e
+
+    return {"reports": reports, "risk_score": risk_score, "link": link}
