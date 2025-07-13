512542c68c9d0476b7f04fe0673b35fd13fd20dc
Jon Mark Allen <ubahmapk@gmail.com>
2025-07-13 14:59:46 -0500

    Refactored for for testing, added pytests and datatypes

--- engines/crtsh.py
+++ engines/crtsh.py
@@ -1,7 +1,13 @@
 import logging
-from typing import Any, Optional
+from collections import Counter
 
 import requests
+from pydantic import ValidationError
+from requests.exceptions import JSONDecodeError, RequestException
+
+from models.crtsh_datamodel import Certificate
+from models.datatypes import ObservableMap, Proxies, Report
+from utils.config import QueryError
 
 logger = logging.getLogger(__name__)
 
@@ -19,72 +25,94 @@ API_KEY_REQUIRED: bool = False
 
 
 def run_engine(
-    observable_dict: dict,
-    proxies: dict[str, str] | None = None,
+    observable_dict: ObservableMap,
+    proxies: Proxies,
     ssl_verify: bool = True,
-) -> Optional[dict[str, Any]]:
+) -> Report | None:
     """
     Queries the crt.sh API for information about a given observable (URL or FQDN).
 
     Args:
-        observable (str): The observable to query.
-        observable_type (str): The type of the observable (e.g., "URL", "IP", "MD5", "SHA256", etc.).
-        proxies (dict): A dictionary of proxies to use for the request.
+        observable (ObservableMap): The observable mapping, including the value and type
+        proxies (Proxies): The proxy servers to use for the request.
+        ssl_verify (bool): TLS verification setting
 
     Returns:
-        dict: A dictionary containing "scan_count", "top_domains", and "link". For example:
-              {
-                  "top_domains": [{"domain": "example.com", "count": 5}, ...],
-                  "link": "https://crt.sh/?q=example.com"
-              }
-        None: If an error occurs.
+        (Report | None) : A dictionary containing "scan_count", "top_domains", and "link", or None
+            if an error occurs.
+            For example:
+            {
+                "top_domains": [{"domain": "example.com", "count": 5}, ...],
+                "link": "https://crt.sh/?q=example.com"
+            }
     """
 
-    observable: str = observable_dict["value"]
+    target: str = observable_dict["value"]
 
     # If observable is a URL, extract domain
-    if (observable_type := observable_dict["type"]) == "URL":
-        domain_part = observable.split("/")[2].split(":")[0]
-        observable = domain_part
+    if (observable_dict["type"]) == "URL":
+        domain_part = target.split("/")[2].split(":")[0]
+        target = domain_part
 
-    url = f"https://crt.sh/json?q={observable}"
+    try:
+        query_results: list[dict] = query_engine(target, proxies, ssl_verify)
+        report: Report = parse_results(query_results, target)
+    except QueryError as e:
+        logger.error(e)
+        return None
+
+    return report
+
+
+def query_engine(target: str, proxies: Proxies, ssl_verify: bool = True) -> list[dict]:
+    url = f"https://crt.sh/json?q={target}"
 
     try:
         response = requests.get(url, proxies=proxies, verify=ssl_verify, timeout=20)
         response.raise_for_status()
 
         results = response.json()
-    except requests.exceptions.RequestException as e:
-        logger.error(
-            "Error querying crt.sh for '%s' (%s): %s",
-            observable,
-            observable_type,
-            e,
-            exc_info=True,
-        )
-        return None
+    except (RequestException, JSONDecodeError) as e:
+        logger.error(f"Error querying crt.sh for {target}: {e}", exc_info=True)
+        raise QueryError from e
 
-    domain_count = {}
-    for entry in results:
+    if len(results) < 1:
+        raise QueryError(f"No results for {target}")
+
+    return results
+
+
+def parse_results(query_results: list[dict], target: str) -> Report:
+    """Parse dict into a list of Certificate objects"""
+    results: list[Certificate] = []
+
+    for certificate in query_results:
+        try:
+            results.append(Certificate(**certificate))
+        except ValidationError:
+            continue
+
+    domain_count: Counter = Counter()
+    for certificate in results:
         domains = set()
 
-        common_name = entry.get("common_name", None)
-        if common_name is not None and len(common_name) != 0:
-            domains.add(common_name)
+        if certificate.common_name:
+            domains.add(certificate.common_name)
 
-        name_value = entry.get("name_value", None)
-        if name_value is not None and len(name_value) != 0:
-            for el in name_value.split("\n"):
+        if certificate.name_value:
+            for el in certificate.name_value.split("\n"):
                 if len(el) > 0:
                     domains.add(str(el).strip())
 
         for domain in domains:
-            domain_count[domain] = domain_count.get(domain, 0) + 1
+            domain_count[domain] += 1
 
     # Sort and extract top 5
-    sorted_domains = sorted(domain_count.items(), key=lambda item: item[1], reverse=True)
-    top_domains = [{"domain": dmn, "count": cnt} for dmn, cnt in sorted_domains[:5]]
-    return {
-        "top_domains": top_domains,
-        "link": f"https://crt.sh/?q={observable}",
-    }
+    sorted_domains: list[tuple[str, int]] = sorted(domain_count.items(), key=lambda item: item[1], reverse=True)
+    top_domains: list[dict[str, str | int]] = [{"domain": dmn, "count": cnt} for dmn, cnt in sorted_domains[:5]]
+    return Report(
+        {
+            "top_domains": top_domains,
+            "link": f"https://crt.sh/?q={target}",
+        }
+    )
