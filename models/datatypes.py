596d140bdc80e83ea3a8ed7999fbb287bfd16bcc
Jon Mark Allen <ubahmapk@gmail.com>
2025-11-22 21:14:58 -0600

    Added the Engine Protocol class

--- models/datatypes.py
+++ models/datatypes.py
@@ -1,4 +1,4 @@
-from typing import Any, TypeAlias, TypedDict
+from typing import Any, Protocol, Self, TypeAlias, TypedDict
 
 
 class ObservableMap(TypedDict):
@@ -9,3 +9,21 @@ class ObservableMap(TypedDict):
 Proxies: TypeAlias = dict[str, str]
 
 Report: TypeAlias = dict[str, Any]
+
+
+class Engine(Protocol):
+    SUPPORTED_OBSERVABLE_TYPES: list[str]
+    NAME: str
+    LABEL: str
+    SUPPORTS: list[str]
+    DESCRIPTION: str
+    COST: str
+    API_KEY_REQUIRED: bool
+    MIGRATED: bool
+
+    def run_engine(
+        self: Self,
+        observable: ObservableMap,
+        proxies: Proxies,
+        ssl_verify: bool,
+    ) -> Report | None: ...
