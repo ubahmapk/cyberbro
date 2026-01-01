840a741e598a2a1e68d94ee92b1d58705beaf81a
stanfrbd <stanfrbd@github.com>
2026-01-01 17:51:58 +0100

    Update to v0.10.0

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ from utils.stats import get_analysis_stats
 from utils.utils import extract_observables
 
 # Canonical version string displayed in the about page and used for update checks
-VERSION: str = "v0.9.9"
+VERSION: str = "v0.10.0"
 
 
 class InvalidCachefileError(Exception):
