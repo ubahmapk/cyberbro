75531cd276936d9060b91e00f0f5395a2ae9bbdb
Stig Dahl <stig@charlottendal.net>
2026-01-08 16:53:07 +0100

    Add test for URL and fix the threat response for url queries

--- tests/test_reversinglabs_spectra_analyze.py
+++ tests/test_reversinglabs_spectra_analyze.py
@@ -15,13 +15,20 @@ API_URL = "https://a1000-example123.reversinglabs.com"
 IP4 = "1.1.1.1"
 IP6 = "fe00::0"
 FQDN = "kosmicband.com"
-URL = "https://datatracker.ietf.org/doc/html/rfc2606"
+URL_UNKNOWN = "https://datatracker.ietf.org/doc/html/rfc2606"
+URL_MALWARE = "https://earsi.com/H1.zip"
 MD5 = "3749f52bb326ae96782b42dc0a97b4c1"
 SHA1 = "3a30948f8cd5655fede389d73b5fecd91251df4a"
 SHA256 = "c67c199595622dfbdc9e415c4a0ad6166eb49cbf74c6aac7bb3e958604d5ecb8"
 
 
 # Tests
+def rlengine_object():
+    """
+    This initiates the base object with dummy data.
+    """
+    rlengine = RLAnalyzeEngine("secret", None, False)
+    return rlengine
 
 
 # Example JSON data from Reversing Labs API response
@@ -62,8 +69,17 @@ def ipv6_response_from_file():
 
 
 @pytest.fixture(scope="session")
-def url_response_from_file():
-    file_response: Path = Path("tests/api_responses/reversinglabs_spectra_analyze/url_api_response.json")
+def url_unknown_response_from_file():
+    file_response: Path = Path("tests/api_responses/reversinglabs_spectra_analyze/url_unknown_api_response.json")
+    with file_response.open() as f:
+        data: dict = json.loads(f.read())
+
+    return data
+
+
+@pytest.fixture(scope="session")
+def url_malware_response_from_file():
+    file_response: Path = Path("tests/api_responses/reversinglabs_spectra_analyze/url_malware_api_response.json")
     with file_response.open() as f:
         data: dict = json.loads(f.read())
 
@@ -162,15 +178,32 @@ def expected_ipv6_report():
 
 
 @pytest.fixture()
-def expected_url_report():
+def expected_url_unknown_report():
     return {
-        "link": f"{API_URL}/url/{quote_plus(URL)}/analysis/url/",
+        "link": f"{API_URL}/url/{quote_plus(URL_UNKNOWN)}/analysis/url/",
         "malicious": 0,
         "report_color": "green",
         "report_type": "network",
         "reports": 21,
         "suspicious": 0,
         "threats": [None, "information_technology", "business_economy"],
+    }
+
+
+@pytest.fixture()
+def expected_url_malware_report():
+    return {
+        "link": f"{API_URL}/url/{quote_plus(URL_MALWARE)}/analysis/url/",
+        "malicious": 7,
+        "report_color": "red",
+        "report_type": "network",
+        "reports": 19,
+        "suspicious": 0,
+        "threats": [
+            "Win32.Ransomware.Xorist",
+            "Web.Hyperlink.Blacklisted",
+            "malware_file",
+        ],
     }
 
 
@@ -181,31 +214,15 @@ def expected_url_report():
         ("IPv4", IP4, f"/api/network-threat-intel/ip/{IP4}/report/"),
         ("IPv6", IP6, f"/api/network-threat-intel/ip/{IP6}/report/"),
         ("FQDN", FQDN, f"/api/network-threat-intel/domain/{FQDN}/"),
-        (
-            "URL",
-            URL,
-            f"/api/network-threat-intel/url/?url={quote_plus(URL)}",
-        ),
-        (
-            "SHA1",
-            SHA1,
-            f"/api/samples/v3/{SHA1}/classification/?av_scanners=1",
-        ),
-        (
-            "MD5",
-            MD5,
-            f"/api/samples/v3/{MD5}/classification/?av_scanners=1",
-        ),
-        (
-            "SHA256",
-            SHA256,
-            f"/api/samples/v3/{SHA256}/classification/?av_scanners=1",
-        ),
+        ("URL", URL_UNKNOWN, f"/api/network-threat-intel/url/?url={quote_plus(URL_UNKNOWN)}"),
+        ("SHA1", SHA1, f"/api/samples/v3/{SHA1}/classification/?av_scanners=1"),
+        ("MD5", MD5, f"/api/samples/v3/{MD5}/classification/?av_scanners=1"),
+        ("SHA256", SHA256, f"/api/samples/v3/{SHA256}/classification/?av_scanners=1"),
         ("NaN", IP4, None),
     ],
 )
 def test_get_api_endpoint(type: str, artifact: str, endpoint: str | None):
-    rlengine = RLAnalyzeEngine("secret", None, False)
+    rlengine: RLAnalyzeEngine = rlengine_object()
     result: str | None = rlengine._get_api_endpoint(artifact, type)
 
     assert endpoint == result
@@ -218,23 +235,15 @@ def test_get_api_endpoint(type: str, artifact: str, endpoint: str | None):
         ("IPv4", IP4, f"/ip/{IP4}/analysis/ip/"),
         ("IPv6", IP6, f"/ip/{IP6}/analysis/ip/"),
         ("FQDN", FQDN, f"/domain/{FQDN}/analysis/domain/"),
-        (
-            "URL",
-            URL,
-            f"/url/{quote_plus(URL)}/analysis/url/",
-        ),
+        ("URL", URL_UNKNOWN, f"/url/{quote_plus(URL_UNKNOWN)}/analysis/url/"),
         ("MD5", MD5, f"/{MD5}/"),
         ("SHA1", SHA1, f"/{SHA1}/"),
-        (
-            "SHA256",
-            SHA256,
-            f"/{SHA256}/",
-        ),
+        ("SHA256", SHA256, f"/{SHA256}/"),
         ("NaN", IP4, None),
     ],
 )
 def test_get_ui_endpoint(type: str, artifact: str, endpoint: str | None):
-    rlengine = RLAnalyzeEngine("secret", None, False)
+    rlengine: RLAnalyzeEngine = rlengine_object()
     result: str | None = rlengine._get_ui_endpoint(artifact, type)
 
     assert endpoint == result
@@ -242,7 +251,7 @@ def test_get_ui_endpoint(type: str, artifact: str, endpoint: str | None):
 
 # Verify testdata
 @pytest.mark.parametrize(
-    "input_query_response,obs,type,url,expected_report",
+    "input_query_response,observable,observable_type,api_url,expected_report",
     [
         ("fqdn_response_from_file", FQDN, "FQDN", API_URL, "expected_fqdn_report"),
         ("hash_response_from_file", MD5, "MD5", API_URL, "expected_md5_report"),
@@ -250,10 +259,11 @@ def test_get_ui_endpoint(type: str, artifact: str, endpoint: str | None):
         ("hash_response_from_file", SHA256, "SHA256", API_URL, "expected_sha256_report"),
         ("ipv4_response_from_file", IP4, "IPv4", API_URL, "expected_ipv4_report"),
         ("ipv6_response_from_file", IP6, "IPv6", API_URL, "expected_ipv6_report"),
-        ("url_response_from_file", URL, "URL", API_URL, "expected_url_report"),
+        ("url_unknown_response_from_file", URL_UNKNOWN, "URL", API_URL, "expected_url_unknown_report"),
+        ("url_malware_response_from_file", URL_MALWARE, "URL", API_URL, "expected_url_malware_report"),
     ],
 )
-def test_parse_rl_response(request: dict, input_query_response: dict, obs: str, type: str, url: str, expected_report: dict):
+def test_parse_rl_response(request: dict, input_query_response: dict, observable: str, observable_type: str, api_url: str, expected_report: dict):
     """
     Use the built-in pytest fixture, request, to take the fixture name
     **as a string** in the parametrized list and retrieve the **actual**
@@ -263,7 +273,7 @@ def test_parse_rl_response(request: dict, input_query_response: dict, obs: str, 
     """
     input: dict = request.getfixturevalue(input_query_response)
     expected: dict = request.getfixturevalue(expected_report)
-    rlengine = RLAnalyzeEngine("secret", None, False)
-    report: dict = rlengine._parse_rl_response(input, obs, type, url)
+    rlengine: RLAnalyzeEngine = rlengine_object()
+    report: dict = rlengine._parse_rl_response(input, observable, observable_type, api_url)
 
     assert report == expected
