0b150f695a800fa6b4277a1103644667273ee734
Jon Mark Allen <ubahmapk@gmail.com>
2026-01-21 11:50:24 -0600

    Update engine to explicitly override BaseEngine. Utilize generic Mapping for parameters for improved flexibility

--- engines/ipinfo.py
+++ engines/ipinfo.py
@@ -1,8 +1,10 @@
 import logging
+from collections.abc import Mapping
 from typing import Any
 
 import pycountry
 import requests
+from typing_extensions import override
 
 from models.base_engine import BaseEngine
 
@@ -11,17 +13,21 @@ logger = logging.getLogger(__name__)
 
 class IPInfoEngine(BaseEngine):
     @property
+    @override
     def name(self):
         return "ipinfo"
 
     @property
+    @override
     def supported_types(self):
         return ["IPv4", "IPv6"]
 
     @property
+    @override
     def execute_after_reverse_dns(self):
         return True  # IP-only engine
 
+    @override
     def analyze(
         self, observable_value: str, observable_type: str
     ) -> dict[str, Any] | None:
@@ -82,7 +88,9 @@ class IPInfoEngine(BaseEngine):
 
         return None
 
-    def create_export_row(self, analysis_result: Any) -> dict:
+    @classmethod
+    @override
+    def create_export_row(cls, analysis_result: Mapping) -> dict:
         if not analysis_result:
             return {f"ipinfo_{k}": None for k in ["cn", "country", "geo", "asn", "org"]}
 
