f957dcedb3087a1f58fdbad2e93e2eccd750d504
Jon Mark Allen <ubahmapk@gmail.com>
2026-01-20 20:03:37 -0600

    type hints

--- models/analysis_result.py
+++ models/analysis_result.py
@@ -4,13 +4,19 @@ db = SQLAlchemy()
 
 
 class AnalysisResult(db.Model):
-    id = db.Column(db.String, primary_key=True)
-    results = db.Column(db.JSON, nullable=False)
-    start_time = db.Column(db.Float, nullable=False)
-    end_time = db.Column(db.Float, nullable=True)  # Permettre les valeurs NULL
-    start_time_string = db.Column(db.String, nullable=False)
-    end_time_string = db.Column(db.String, nullable=True)  # Permettre les valeurs NULL
-    analysis_duration_string = db.Column(db.String, nullable=True)  # Permettre les valeurs NULL
-    analysis_duration = db.Column(db.Float, nullable=True)  # Permettre les valeurs NULL
-    selected_engines = db.Column(db.JSON, nullable=False)
-    in_progress = db.Column(db.Boolean, default=True)
+    id: str = db.Column(db.String, primary_key=True)
+    results: dict = db.Column(db.JSON, nullable=False)
+    start_time: float = db.Column(db.Float, nullable=False)
+    end_time: float = db.Column(db.Float, nullable=True)  # Permettre les valeurs NULL
+    start_time_string: str = db.Column(db.String, nullable=False)
+    end_time_string: str = db.Column(
+        db.String, nullable=True
+    )  # Permettre les valeurs NULL
+    analysis_duration_string: str = db.Column(
+        db.String, nullable=True
+    )  # Permettre les valeurs NULL
+    analysis_duration: float = db.Column(
+        db.Float, nullable=True
+    )  # Permettre les valeurs NULL
+    selected_engines: dict = db.Column(db.JSON, nullable=False)
+    in_progress: bool = db.Column(db.Boolean, default=True)
