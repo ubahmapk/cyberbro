45b90ceaeae2fda79988cad81670a83efed522d1
Jon Mark Allen <ubahmapk@gmail.com>
2026-01-25 16:09:33 -0600

    test(ipapi): add fixtures and functional tests; add fakerx to dev requirements

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,3 +19,32 @@ def url_observable_dict():
 @pytest.fixture()
 def api_key() -> str:
     return "test_api_key"
+
+
+# Additional fixtures for IPAPI tests
+@pytest.fixture()
+def secrets_with_key():
+    from utils.config import Secrets
+
+    s = Secrets()
+    s.ipapi = "X" * 20
+    return s
+
+
+@pytest.fixture()
+def secrets_without_key():
+    from utils.config import Secrets
+
+    s = Secrets()
+    s.ipapi = ""
+    return s
+
+
+@pytest.fixture()
+def ipv4_observable():
+    return "1.1.1.1"
+
+
+@pytest.fixture()
+def ipv6_observable():
+    return "2001:4860:4860::8888"
