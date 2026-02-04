107eeec1b7638eb33703e3fce3b5368d605a6410
Jon Mark Allen <ubahmapk@gmail.com>
2026-02-03 19:00:12 -0600

    Updated base_engine with ObservableType
    Moved observable dataclasses to dedicated model file

---
+++ models/observable.py
@@ -0,0 +1,45 @@
+from enum import Flag, auto
+
+from pydantic import AnyUrl, ValidationError
+from pydantic.dataclasses import dataclass
+
+
+class ObservableType(Flag):
+    CHROME_EXTENSION = auto()
+    EMAIL = auto()
+    FQDN = auto()
+    IPV4 = auto()
+    IPV6 = auto()
+    MD5 = auto()
+    SHA1 = auto()
+    SHA256 = auto()
+    URL = auto()
+    BOGON = auto()
+
+
+@dataclass(slots=True)
+class Observable:
+    """Represents an observable value with a type and value.
+
+    Attributes:
+        type (ObservableType): The type of the observable.
+        value (str): The value of the observable.
+
+    Not yet used.
+    """
+
+    type: ObservableType
+    value: str
+
+    def __hash__(self) -> int:
+        """Set membership requires the object to be hashable.
+        Use a tuple of the type and value to ensure uniqueness.
+        """
+        return hash((self.value, self.type))
+
+    def _return_fqdn_from_url(self) -> str:
+        """Return an empty string if pydantic validation fails or if no host is present."""
+        try:
+            return AnyUrl(self.value).host or ""
+        except ValidationError:
+            return ""
